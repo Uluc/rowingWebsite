c50cd57be86b62c0e7ba743fa6efb61a9c72b3a8
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-04-28 16:43:59 -0500

    static folder made, base created

--- rowingWebsite/settings.py
+++ rowingWebsite/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rowingWebsiteApp.apps.RowingwebsiteappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
--- rowingWebsiteApp/views.py
+++ rowingWebsiteApp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Website Home</h1>')
+    return render(request, 'rowingWebsiteApp/homePage.htm')
 
 def roster(request):
-    return HttpResponse('<h1>Website Roster</h1>')
+    return render(request, 'rowingWebsiteApp/rosterPage.htm')
