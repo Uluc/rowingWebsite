d366bcc9dbf1d57a7d67394b8ef41043f227c9dd
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-05-03 17:45:54 -0500

    naviagation bar working

--- rowingWebsiteApp/views.py
+++ rowingWebsiteApp/views.py
@@ -3,19 +3,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'rowingWebsiteApp/homePage.htm')
+    return render(request, 'rowingWebsiteApp/homePage.htm', {'nbar': 'home'})
 
 def roster(request):
-    return render(request, 'rowingWebsiteApp/rosterPage.htm')
+    return render(request, 'rowingWebsiteApp/rosterPage.htm', {'nbar': 'roster'})
 
 def schedule(request):
-    return render(request, 'rowingWebsiteApp/schedulePage.htm')
+    return render(request, 'rowingWebsiteApp/schedulePage.htm', {'nbar': 'schedule'} )
     
 def sponsor(request):
-    return render(request, 'rowingWebsiteApp/sponsorPage.htm')
+    return render(request, 'rowingWebsiteApp/sponsorPage.htm', {'nbar': 'sponsor'})
 
 def recruitment(request):
-    return render(request, 'rowingWebsiteApp/recruitmentPage.htm')
+    return render(request, 'rowingWebsiteApp/recruitmentPage.htm', {'nbar': 'recruitment'})
 
 def leadership(request):
-    return render(request, 'rowingWebsiteApp/leadershipPage.htm')
+    return render(request, 'rowingWebsiteApp/leadershipPage.htm', {'nbar': 'leadership'})
