5b535f978736c1cbc23c26b378f806eabe0377b4
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-05-06 02:12:10 -0500

    model for rower created

--- rowingWebsiteApp/models.py
+++ rowingWebsiteApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Rowers(models.Model):
+    name = models.TextField()
+    orientation = models.TextField()
+    hometown = models.TextField()
+    major = models.TextField()
