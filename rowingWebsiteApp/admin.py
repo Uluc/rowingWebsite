77206ed9f7fc8aa45d42d1699032d475026a47b3
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-05-11 19:56:30 -0500

    admin setup for cropped images

--- rowingWebsiteApp/admin.py
+++ rowingWebsiteApp/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Rower, Race, Leadership, Event
+from image_cropping import ImageCroppingMixin
 
-admin.site.register(Rower)
+class MyModelAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Rower, MyModelAdmin)
 admin.site.register(Race)
-admin.site.register(Leadership)
+admin.site.register(Leadership, MyModelAdmin)
 admin.site.register(Event)
 # Register your models here.
