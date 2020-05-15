25c169a31affe929a578b73a50ba301923d5d545
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-05-15 03:21:23 -0500

    gallery added to admin

--- rowingWebsiteApp/admin.py
+++ rowingWebsiteApp/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Rower, Race, Leadership, Event
-from image_cropping import ImageCroppingMixin
+from .models import Rower, Race, Leadership, Event, Sponsors, Gallery
+from image_cropping.admin import ImageCroppingMixin
+from adminsortable2.admin import SortableAdminMixin
 
 class MyModelAdmin(ImageCroppingMixin, admin.ModelAdmin):
     pass
 
+class MyModelAdminSorting(SortableAdminMixin, ImageCroppingMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Sponsors)
 admin.site.register(Rower, MyModelAdmin)
 admin.site.register(Race)
-admin.site.register(Leadership, MyModelAdmin)
+admin.site.register(Leadership, MyModelAdminSorting)
 admin.site.register(Event)
+admin.site.register(Gallery, MyModelAdmin)
+
 # Register your models here.
