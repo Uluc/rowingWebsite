23e1775162a0532d6c72e7ae72b527f1989073e1
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2020-06-01 04:38:30 -0500

    all pages implemented

--- rowingWebsiteApp/views.py
+++ rowingWebsiteApp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Race, Leadership, Event, Rower
+from .models import Race, Leadership, Event, Rower, Sponsor, Picture, Gallery, SponsorPage, HomePage, AboutPage, RecruitmentPage, PageBanners
 from django.forms.models import model_to_dict
-import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.fields.files import ImageFieldFile
-from image_cropping.utils import get_backend
+from django.db.models import F  
+from django.core.mail import send_mail
+
+
+
 # Create your views here.
 
 class ExtendedEncoder(DjangoJSONEncoder):
@@ -15,33 +18,84 @@ class ExtendedEncoder(DjangoJSONEncoder):
         else:
             return super().default(o)
 
-def home(request):
-    return render(request, 'rowingWebsiteApp/homePage.htm', {'nbar': 'home'})
+def aboutus(request):
+    data={
+        'nbar' : 'aboutus',
+        'aboutText' : AboutPage.objects.all(),
+        'banner' : PageBanners.objects.all()
+    }
+    return render(request, 'rowingWebsiteApp/aboutUsPage.htm', data)
 
+def confirmed(request):
+    return render(request, 'rowingWebsiteApp/confirmationPage.htm')
+
+def home(request):
+    nbar = 'home'
+    homeText = HomePage.objects.all()
+
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        send_mail(
+            'Inquiry from ' + message_name,
+            message,
+            message_email,
+            ['ulucozdenvar01@gmail.com'],
+        )
+
+        return render(request, 'rowingWebsiteApp/homePage.htm', {'nbar':nbar, 'homeText':homeText})
+    else:
+        return render(request, 'rowingWebsiteApp/homePage.htm', {'nbar':nbar, 'homeText':homeText})
+
+def photogallery(request):
+    data = {
+        'nbar' : 'photos',
+        'gallery' : Gallery.objects.all()
+    }
+    return render(request, 'rowingWebsiteApp/photoGalleryPage.htm', data )
 
 def roster(request):
-    nbar = 'roster'
-    
-    rower = Rower.objects.all()
-    rowers = json.dumps([model_to_dict(item) for item in rower], cls=ExtendedEncoder)
-
-    return render(request, 'rowingWebsiteApp/rosterPage.htm', {'nbar':nbar, 'rowers':rowers, 'rower':rower} )
+    data={
+        'nbar' : 'roster' , 
+        'rower' : Rower.objects.all()
+    }
+    return render(request, 'rowingWebsiteApp/rosterPage.htm', data)
 
 def schedule(request):
-    nbar = 'schedule'
-    races = json.dumps([model_to_dict(item) for item in Race.objects.all()])
-    return render(request, 'rowingWebsiteApp/schedulePage.htm',{'nbar':nbar, 'races':races} )
+    data = {
+        'nbar' : 'schedule',
+        'races' : Race.objects.all().order_by(F('date').asc(nulls_last=True)),
+        'banner' : PageBanners.objects.all()
+    }
+    return render(request, 'rowingWebsiteApp/schedulePage.htm', data)
     
 def sponsor(request):
-    return render(request, 'rowingWebsiteApp/sponsorPage.htm', {'nbar': 'sponsor'})
+    data = {
+        'nbar' : 'sponsor',
+        'sponsors' : Sponsor.objects.all(),
+        'sponsorText' : SponsorPage.objects.all(),
+        'banner' : PageBanners.objects.all()
+    }
+    return render(request, 'rowingWebsiteApp/sponsorPage.htm', data)
 
 def recruitment(request):
-    nbar = 'recruitment'
-    events = json.dumps([model_to_dict(item) for item in Event.objects.all()])
-    return render(request, 'rowingWebsiteApp/recruitmentPage.htm', {'nbar':nbar, 'events':events})
+    data = {
+        'nbar' : 'recruitment',
+        'events' : Event.objects.all(),
+        'recruitmentText' : RecruitmentPage.objects.all(),
+        'banner' : PageBanners.objects.all()
+        
+    }
+    return render(request, 'rowingWebsiteApp/recruitmentPage.htm', data)
 
 def leadership(request):
-    nbar = 'leadership'
-    officers = json.dumps([model_to_dict(item) for item in Leadership.objects.all()], cls=ExtendedEncoder)
-    return render(request, 'rowingWebsiteApp/leadershipPage.htm', {'nbar':nbar, 'officers': officers} )
+    data = {
+        'nbar' : 'leadership',
+        'leadership' : Leadership.objects.all()
+    }
+    return render(request, 'rowingWebsiteApp/leadershipPage.htm', data )
+
+
 
